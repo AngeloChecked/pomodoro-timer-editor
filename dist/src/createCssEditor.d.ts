8948739eb9fcbbf2d9db6b4b75cbde53dd8f5fa3
angelo <angi99552@gmail.com>
2022-08-04 16:58:29 +0200

    build new website version

--- dist/src/createCssEditor.d.ts
+++ dist/src/createCssEditor.d.ts
@@ -1,2 +1,3 @@
 import * as monaco from 'monaco-editor';
 export default function createCssEditor(htmlElement: HTMLElement, cssStyle: HTMLElement): monaco.editor.IStandaloneCodeEditor;
+export declare function loadCustomCss(editorSelectStyles: HTMLSelectElement, cssEditor: monaco.editor.IStandaloneCodeEditor): void;
